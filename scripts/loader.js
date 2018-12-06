b6e2cdf6af6c302f2b9bd6ed3fc39d50e79f9f70
Kent Madsen <20760795+KentMadsen@users.noreply.github.com>
2018-12-06 09:28:10 +0100

    current iteration

--- scripts/loader.js
+++ scripts/loader.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d6b9c6ef790324044fc4b7a2a365196c8b934890fe2c118fc33a64e1a93fa64f
-size 414
+oid sha256:86ea0ed6b6c13f3dcd3af105a185aa6113e86aacee049c27ef26d9d63ba93710
+size 1675
