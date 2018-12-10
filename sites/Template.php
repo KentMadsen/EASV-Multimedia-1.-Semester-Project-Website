cfeb5c35c15cbd3a9863a6b14d222c8b27208b42
Kent Madsen <20760795+KentMadsen@users.noreply.github.com>
2018-12-10 09:27:47 +0100

    template added

---
+++ sites/Template.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+
+        <meta name="viewport"
+              content="width=device-width, initial-scale=1.0">
+
+        <meta http-equiv="X-UA-Compatible"
+              content="ie=edge">
+
+        <?php
+            include './generics/Fonts.html';
+            include './generics/Content.html';
+            include './generics/Libraries.html';
+            include './generics/Style.html';
+        ?>
+
+        <title>
+            Title
+        </title>
+    </head>
+    
+    <body>
+        <?php include './generics/Header.html'; ?>
+        <main>
+        
+
+        </main>
+        <?php include './generics/Footer.html'; ?>
+    </body>
+</html>
