68ce58d8e5eb155581229a79e88daa24838d5b7f
Kent Madsen <20760795+KentMadsen@users.noreply.github.com>
2018-12-11 16:00:58 +0100

    redundant

--- sites/script/scripts.js
+++ sites/script/scripts.js
@@ -1,11 +1,13 @@
 function select_page( name_of_header )
 {
-    var x = document.getElementsByClassName(name_of_header);
+    var x = document.getElementsByClassName( name_of_header );
     var i;
 
-    for(i = 0; i < x.length; i++)
+    for( i = 0; 
+         i < x.length;
+         i ++ )
     {
-        x[i].classList.add("selected_page");
+        x[i].classList.add( "selected_page" );
     }
 }
 
