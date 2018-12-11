3622716655b9fad7def0194545350ccd096c374e
Kent Madsen <20760795+KentMadsen@users.noreply.github.com>
2018-12-11 15:36:48 +0100

    redundant

--- sites/script/scripts.js
+++ sites/script/scripts.js
@@ -4,8 +4,3 @@ function select_page( name_of_header )
     id.classList.add("selected");
 }
 
-function show_header()
-{
-    var id = document.getElementById("header_area");
-    id.classList.add("overlay");
-}
