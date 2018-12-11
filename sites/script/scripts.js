729d7f9dc5f2b0240d6474696dabc22feae8c7e9
Kent Madsen <20760795+KentMadsen@users.noreply.github.com>
2018-12-11 20:47:53 +0100

    redundant

--- sites/script/scripts.js
+++ sites/script/scripts.js
@@ -42,6 +42,7 @@ function overlay_click()
 
         for(i =0; i < x.length; i++)
         {
+            console.log('added');
             x[i].classList.add('show');
         }
 
@@ -58,6 +59,7 @@ function overlay_click()
  
          for(i =0; i < x.length; i++)
          {
+             console.log('removed');
              x[i].classList.remove('show');
          }
 
